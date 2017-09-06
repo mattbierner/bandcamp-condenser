4f25b3290355670ea42049eda8d1439da50b902f
Matt Bierner <mattbierner@gmail.com>
2017-09-06 10:15:14 -0700

    pull more tags

--- main.js
+++ main.js
@@ -11,39 +11,96 @@ const outFile = './out.data'
 const tagsConcurrency = 1
 const albumConcurrency = 1
 
+// Some of these tags are noisy but we need all the data we can get
 const tags = [
-    'vaporwave',
-    'vaporambient',
-    'vapornoise',
-    'vaportrap',
-    'post-vaporwave',
-    'nuwrld',
+    'alternative-vaporwave',
+    'ambient-vaporwave',
+    'ambientwave',
+    'babewave',
+    'boguscollective',
+    'chillwave',
+    'computergaze',
+    'doswave',
+    'dream-music',
+    'dreampunk',
+    'eccojams',
+    'faux-utopian',
+    'florida-ambient',
+    'future-funk',
+    'glitchwave',
+    'glo-fi',
+    'hypnagogic-pop',
+    'hypnagogic',
     'internet',
-    'post-internet',
-    'plunderphonics',
-    'nu disco',
+    'late-night-lo-fi',
     'mallsoft',
-    'virtual reality',
-    'dream music'
+    'non-standard',
+    'nu-disco',
+    'nuwrld',
+    'phaserwave',
+    'plunderphonics',
+    'post-internet',
+    'post-vaporwave',
+    'post-xerox',
+    'power_lunch',
+    'savusavu',
+    'seapunk',
+    'signalwave',
+    'slushwave',
+    'tapejams',
+    'vaporambient',
+    'vaporchill',
+    'vaporfunk',
+    'vaporglitch',
+    'vaporhop',
+    'vapornoise',
+    'vaporsex',
+    'vaporsynth',
+    'vaportech',
+    'vaportrap',
+    'vaporwave',
+    'vapour',
+    'vhswave',
+    'virtual-reality',
+    'webpunk',
+    'webwave',
+    'xerox',
 ]
 
+const delay = time =>
+    new Promise(resolve => setTimeout(resolve, time))
 
 
-const getAlbum = promisify(bandcamp.getAlbumInfo)
+const getAlbum = (url, retry = 3) => 
+    promisify(bandcamp.getAlbumInfo)(url).then(x => {
+        if (x && x.tracks && x.tracks.length) {
+            return x
+        }
+        if (!retry) {
+            return x
+        }
+        return new Promise(res => setTimeout(res, 2000))
+            .then(() => getAlbum(url, retry - 1))
+    })
 
 const getAlbumsWithTag = (tag, allAlbums) =>
     new Promise(resolve =>
-        (function loop(page) {
+        (function loop(page, retry = 3) {
             bandcamp.tag({ tag, page }, (error, searchResults) => {
                 if (error) {
                     console.error(error)
                     resolve(allAlbums)
                 } else if (searchResults.length === 0) {
-                    resolve(allAlbums)
+                    if (!retry) {
+                        resolve(allAlbums)
+                        return;
+                    }
+                    setTimeout(() => loop(page, retry - 1), 2000)
                 } else {
                     for (const album of searchResults) {
                         allAlbums.set(album.name, album)
                     }
+                    console.log(tag + '       found ' + searchResults.length)
                     loop(page + 1)
                 }
             })
@@ -60,16 +117,14 @@ ${tracks}`
 }
 
 
-fs.writeFileSync(outFile, '')
-
 const allAlbums = new Map()
 
 pMap(tags, tag => {
     return getAlbumsWithTag(tag, allAlbums)
 }, { concurrency: tagsConcurrency })
     .then(() => {
+        fs.writeFileSync('albums', JSON.stringify(Array.from(allAlbums.values())), 'utf-8')
         return pMap(allAlbums.values(), albumInfo => {
-            console.log(albumInfo.url)
             return getAlbum(albumInfo.url)
                 .then(result => {
                     if (result.tracks) {
@@ -80,4 +135,3 @@ pMap(tags, tag => {
                 })
         }, { concurrency: albumConcurrency })
     })
-
